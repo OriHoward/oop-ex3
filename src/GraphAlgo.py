f19060a05854c7108169ee10e74b7823f6a92f7f
EitanKats <eitan.kats96@gmail.com>
2021-12-17 15:50:01 +0200

    we are reached a point where we can start working

---
+++ src/GraphAlgo.py
@@ -0,0 +1,39 @@
+from typing import List
+from types import SimpleNamespace
+from DiGraph import DiGraph
+from GraphAlgoInterface import GraphAlgoInterface
+from GraphInterface import GraphInterface
+
+
+class GraphAlgo(GraphAlgoInterface):
+
+    def load_from_json(self, file_name: str) -> bool:
+        self.graph = DiGraph()
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        return 0, 0
+
+    def plot_graph(self) -> None:
+        pass
+
+    def get_graph(self) -> GraphInterface:
+        return self.graph
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        """
+        Finds the shortest path that visits all the nodes in the list
+        :param node_lst: A list of nodes id's
+        :return: A list of the nodes id's in the path, and the overall distance
+        """
+
+    def centerPoint(self) -> (int, float):
+        """
+        Finds the node that has the shortest distance to it's farthest node.
+        :return: The nodes id, min-maximum distance
+        """
+
+    def __init__(self, graph=None):
+        self.graph = graph
