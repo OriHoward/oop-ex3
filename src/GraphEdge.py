2a9df049b4132c86205757529d9c312b93c6fc66
OriHoward <ori.howard@gmail.com>
2021-12-20 10:17:37 +0200

    added functions in Node class and edge class

--- src/GraphEdge.py
+++ src/GraphEdge.py
@@ -4,6 +4,15 @@ class GraphEdge:
     weight: float
 
     def __init__(self, src, dest, weight):
-        self.src = src
-        self.dest = dest
-        self.weight = weight
+        self._src = src
+        self._dest = dest
+        self._weight = weight
+
+    def get_src(self):
+        return self._src
+
+    def get_dest(self):
+        return self._dest
+
+    def get_weight(self):
+        return self._weight
--- src/GraphNode.py
+++ src/GraphNode.py
@@ -10,7 +10,33 @@ class GraphNode:
     dist: float
 
     def __init__(self, position: Position, _id: int):
-        self.srcMap = {}
-        self.destMap = {}
-        self.position = Position()
+        self._srcMap = {}
+        self._destMap = {}
+        self._position = Position(position.get_x(), position.get_y(), position.get_z())
         self._id = _id
+
+    def get_srcMap(self):
+        return self._srcMap
+
+    def get_destMap(self):
+        return self._destMap
+
+    def get_key(self):
+        return self._id
+
+    def add_dest(self, edge: GraphEdge):
+        self._destMap[edge.get_dest()] = edge
+
+    def add_src(self,edge: GraphEdge):
+        self._srcMap[edge.get_src()] = edge
+
+    def remove_dest(self, dest: int):
+        return self._destMap.pop(dest)
+
+    def remove_src(self, src: int):
+        return self._srcMap.pop(src)
+
+
+
+
+
