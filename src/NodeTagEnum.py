17d1c1243e15fafb2f82ddcc4c74ed242c561f4c
EitanKats <eitan.kats96@gmail.com>
2021-12-23 20:50:38 +0200

    minor fixed + updated part of the UML

--- src/NodeTagEnum.py
+++ src/NodeTagEnum.py
@@ -5,13 +5,3 @@ class NodeTag(Enum):
     WHITE = 0
     GRAY = 1
     BLACK = 2
-
-
-# examples- how to use
-print(NodeTag.GRAY)
-print(NodeTag.GRAY.name)
-print(NodeTag.GRAY.value)
-print(NodeTag.WHITE == NodeTag.BLACK)
-print(NodeTag.WHITE == NodeTag.WHITE)
-print(NodeTag.WHITE.value == NodeTag.WHITE.value)
-print(NodeTag.WHITE.value == NodeTag.GRAY.value)
